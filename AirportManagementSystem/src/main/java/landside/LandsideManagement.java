0a1a3e681cb67678e9b72350724a8e2bac15bc19
shaslhofer <Ch.li.lu.se>
2022-01-10 21:39:03 +0100

    driver implementation

--- AirportManagementSystem/src/main/java/landside/LandsideManagement.java
+++ AirportManagementSystem/src/main/java/landside/LandsideManagement.java
@@ -1,7 +1,24 @@
 package landside;
 
+import landside.model.destination.Destination;
+import landside.model.vehicle.Vehicle;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.java.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Stefan Haslhofer
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Log
 public class LandsideManagement {
+    private List<Vehicle> vehicles = new ArrayList<>();
+    private List<Destination> destinations = new ArrayList<>();
 }
--- AirportManagementSystem/src/main/java/landside/model/Driver.java
+++ AirportManagementSystem/src/main/java/landside/model/Driver.java
@@ -1,30 +1,41 @@
 package landside.model;
 
+import landside.model.destination.Destination;
+import landside.model.destination.FuelDepot;
+import landside.model.vehicle.Vehicle;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.java.Log;
 
 @Getter
 @Setter
+@Log
 public class Driver {
     private int licenseId;
+    private Vehicle vehicle;
 
-    public void driveToDestination() {
-
+    public void driveToDestination(Destination dest) {
+        vehicle.setDestination(dest);
+        log.info("Driving to destination...");
     }
 
     public void driveToGarage(){
-
+        vehicle.setDestination(null);
+        vehicle.setInGarage(true);
+        log.info("Driving back to garage...");
     }
 
     public void reportStatus() {
 
     }
 
-    public void driveToFuelDepot() {
-
+    public void driveToFuelDepot(FuelDepot depot) {
+        vehicle.setDestination(depot);
+        log.info("Driving to fuel depot...");
     }
 
     public void refuelVehicle() {
+        vehicle.openFuelCap();
 
     }
 }
